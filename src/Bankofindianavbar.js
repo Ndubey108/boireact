535c51a8f964e9726de4858d30105d5cd23ac24a
Nidhi Dubey <93912780+Ndubey108@users.noreply.github.com>
2023-05-31 13:45:33 +0530

    Update Bankofindianavbar.js

--- src/Bankofindianavbar.js
+++ src/Bankofindianavbar.js
@@ -1,20 +1,17 @@
 import React from 'react';
 import './App.css';
 import {Link} from 'react-router-dom';
-import Aboutus from './Components/Aboutus';
-import Boiweb from './Components/Boiweb';
-import Investor from './Components/Investor';
-import Forex from './Components/Forex';
+
 const Bankofindianavbar=()=>{
-  const name="nidhi"
+ 
 return(
 <header>
         <nav  className="nav">
             <ul className="nav-items">
-                <li className="item"><Link  className="Link" to="/" element={<Aboutus/>}>About Us</Link></li>
-                <li className="item"><Link className="Link"to="/boiweb" element={<Boiweb/>}>BOI Web</Link></li>
-                <li className="item"><Link  className="Link" to="/investcorner" element={<Investor/>}>Investor Corner</Link></li>
-                <li className="item"><Link  className="Link" to="/forexcardrate" element={<Forex name={name}/>}>Forex Card Rate</Link></li>
+                <li className="item"><Link  className="Link" to="/about" >About Us</Link></li>
+                <li className="item"><Link className="Link"to="/boiweb" >BOI Web</Link></li>
+                <li className="item"><Link  className="Link" to="/investcorner" >Investor Corner</Link></li>
+                <li className="item"><Link  className="Link" to="/forexcardrate" >Forex Card Rate</Link></li>
                 <li className="item"> <Link  className="Link"to="#"><i className="fa fa-phone"></i>1800 103 1906</Link></li> 
                  <li className="item">
                   <input type="text" value="English"></input><button id="dropdownbtn"><img src="./images/down-arrow.png" id="firstarrow" height="10px" width="20px"></img></button>
